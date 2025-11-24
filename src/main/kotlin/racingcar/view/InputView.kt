eecf056b92b6a566508254c184412182c3cf7fe7
daun <wjdekdns0218@gmail.com>
2025-11-24 14:44:54 +0900

    feat(view): 자동차 이름과 시도 횟수 입력 기능 구현

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,13 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun carNameInput() : String {
+        return Console.readLine()
+    }
+
+    fun attemptCountInput() : String {
+        return Console.readLine()
+    }
+}
